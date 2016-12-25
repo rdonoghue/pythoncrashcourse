0b09e0852fdee610e258e04c452ec38fdcd59de8
Rob Donoghue <rdonoghue@gmail.com>
2016-12-25 18:19:57 -0500

    Finished with dictionaries and the UTTERLY BULLSHIT example which is also filling me with hate towards python 3 as a side effect.

--- dictionaries.py
+++ dictionaries.py
@@ -1,1 +1,65 @@
-dictionaries
+clothes0= {
+	'shirt' : 'lavender', 
+	'pants' : 'blue', 	
+	'hat' : 'blue'
+	}
+
+clothes1= {
+	'shirt' : 'blue', 
+	'pants' : 'red', 	
+	'hat' : 'yellow'
+	}
+clothes2= {
+	'shirt' : 'turquoise', 
+	'pants' : 'green', 	
+	'hat' : 'black'
+	}
+
+allclothes = [clothes0, clothes1, clothes2]
+
+for aparrel in allclothes:
+	print aparrel
+
+
+'''
+
+Ok, hiding all this while I mess with the next bit
+
+clothes1['socks'] = 'purple'
+
+print(clothes1)
+print
+print(clothes1['socks'])
+print
+clothes1['socks'] = 'orange'
+print(clothes1['socks'])
+print
+del clothes1['socks']
+print(clothes1)
+
+for x,y in clothes1.items():
+	print(x),
+	print("\t:"),
+	print(y)
+# ^^^ that items() seems to be the key. Just doing clothes1 throws an error
+
+print
+# keys() will get just keys
+for x in clothes1.keys():
+	print(x)
+print
+# Huh. Apparently keys() is the default pehavior
+for x in clothes1:
+	print(x)
+print
+
+# Alternately, we hit values()
+for x in clothes1.values():
+	print(x)
+print
+
+# We an also pre-sort
+for x in sorted(clothes1.keys()):
+	print(x)
+print
+'''
---
+++ dictionaries3.py
@@ -0,0 +1,31 @@
+# lists in a dictionary
+clothes1= {
+	'shirt' : 'blue', 
+	'pants' : 'red', 	
+	'hat' : ['yellow', 'black']
+	}
+
+
+for x,y in clothes1.items():
+	print(x.title() + ":")
+#	print(len(y)),
+#	print(y)
+	isstring=isinstance(y,basestring)
+	if isstring:
+		print("\t" + y.title())
+	else:
+		for z in y:
+			print("\t" + z.title())
+	
+# I am >< to installing python 3 to see if this is one of those cases
+# Ok, fuck it, installing Python 3 to see if this runs differently
+
+# NO IT STILL FUCKING DOES THIS THING
+
+# GOD DAMMIT YOU CHEATING SON OF A BITCH
+# Ok, Added the fucking isintance check after digging around stackoverflow 
+# because the example in the book is a CHEATING ASS PIECE OF SHIT
+
+
+# I could put a dictionary in a dictionary now, but honest to god I think
+# I'm just too pissed to even try. 
